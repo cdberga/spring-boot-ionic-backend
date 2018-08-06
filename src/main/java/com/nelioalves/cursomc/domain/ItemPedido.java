64fbf4d2cc1968b038c94a1cdf313f05f5897ea6
Carlos <carlos.bergamasco@gmail.com>
2018-08-06 16:09:11 -0300

    Inserindo Pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -31,7 +31,7 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
+
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
@@ -41,8 +41,16 @@ public class ItemPedido implements Serializable {
 		return id.getPedido();
 	}
 
+	public void setPedido(Pedido p) {
+		id.setPedido(p);
+	}
+
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+	
+	public void setProduto(Produto p) {
+		id.setProduto(p);
 	}
 
 	public ItemPedidoPK getId() {
