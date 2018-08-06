de66ae90d5f69dc88b8588bd38523abe7eefe010
Carlos <carlos.bergamasco@gmail.com>
2018-08-06 14:25:25 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/nelioalves/cursomc/dto/ProdutoDto.java
@@ -0,0 +1,54 @@
+package com.nelioalves.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Produto;
+
+public class ProdutoDto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private Double preco;
+
+	public ProdutoDto() {
+
+	}
+
+	public ProdutoDto(Produto obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		preco = obj.getPreco();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
